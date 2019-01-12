5c8c87a0759c85e423d93b1f7053a8e8f63a37f1
Breno Nahuz <35977737+bnahuz@users.noreply.github.com>
2019-01-12 20:40:05 -0300

    Adicionado o critério da convergência

--- Ponto Fixo.py
+++ Ponto Fixo.py
@@ -6,17 +6,19 @@ def ponto_fixo():
     n = int(input("Numéro máximo de iterações: " ))
 
     k = 1
-
-    while k <= n:
-        r = g(x)
-        if abs(r-x) < E:
-            print("A raiz pelo método do Ponto Fixo é: ",r)
-            break
-        k += 1
-        r = x
-        if k > n:
-            print("O método falhou após", n,"iterações.")
-            break
+    if dg(x) > 1:
+        print("Ocorre divergência para: ", x)
+    else:
+        while k <= n:
+            r = g(x)
+            if abs(r-x) < E:
+                print("A raiz pelo método do Ponto Fixo é: ",r)
+                break
+            k += 1
+            r = x
+            if k > n:
+                print("O método falhou após", n,"iterações.")
+                break
 
 g = lambda x: 1 - math.cos(x)
 dg = lambda x : math.sin(x)
