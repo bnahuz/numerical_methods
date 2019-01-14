270fa957bac63c8c91bb073660ef36e5975c7ec3
Breno Nahuz <35977737+bnahuz@users.noreply.github.com>
2019-01-13 22:33:13 -0300

    Update Bissecao.py

--- Bissecao.py
+++ Bissecao.py
@@ -1,5 +1,17 @@
 import math
 def max_interacoes(a0,b0,E): 
+     """
+        max_interacoes funciona de acordo com elementos previamente
+        dados para retornar o valor mínimo de iterações necessárias
+        para a aproximação da raiz de acordo com a fórmula:
+            n >= log(a - b) - log (E) / log(2)
+      Argumentos:
+          a0: Início do intervalo
+          b0: Final do intervalo
+          E:  Erro considerado
+      Retorna:
+          Um n(número mínimo) de iterações necessárias.
+    """
     conta = (math.log10(b0-a0)-math.log10(E))/math.log10(2)
     res = int(math.ceil(conta))
     return res
@@ -8,8 +20,8 @@ def bissecao():
     a = float(input("Extremo inicial: " ))
     b = float(input("Extremo final: " ))
     E = float(input("Erro em forma decimal: " ))
-    maxi = max_interacoes(a,b,E)
-    print('O número mínimo de iterações para esta função é de: ',maxi)
+    max_i = max_interacoes(a,b,E)
+    print('O número mínimo de iterações para esta função é de: ',max_i)
     n = int(input("Numéro máximo de iterações: " ))
     i = 1
     Kzao = f(a)
