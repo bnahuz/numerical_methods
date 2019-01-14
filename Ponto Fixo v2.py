3ae8fa5ad10c798c2c8f63412923180480b9e9ab
Breno Nahuz <35977737+bnahuz@users.noreply.github.com>
2019-01-14 20:10:31 -0300

    Update Ponto Fixo v2.py

--- Ponto Fixo v2.py
+++ Ponto Fixo v2.py
@@ -1,34 +1,37 @@
-'''
-Adaptado de: 
-    Numerical Methods for Mathematics, Science and Engineering, [Graphics:../Images/FixedPointProg_gr_1.gif] Ed, 1992
-    John H. Mathews
-    Prentice-Hall, Inc
-    ISBN:  0-13-624990-6 
-'''
-
 import math
 
+def dg(x):
+  h = 10**-10
+  dg = (g(x + h) - g(x))/h
+  return dg
+
 def ponto_fixo():
-    x = float(input("Aproximação: " ))
-    Err = float(input("Erro em forma decimal: " ))
-    max_int = int(input("Numéro máximo de iterações: " ))
-    Erel = 1 # Erro Relativo inicial
-    k = 1 #Contador
+  x = float(input("Aproximação: " ))
+  Err = float(input("Erro em forma decimal: " ))
+  max_int = int(input("Numéro máximo de iterações: " ))
+  k = 1 #Contador
 
-    p_novo = g(x) #Primeira iteração
+  p_novo = g(x) #Primeira iteração
 
-    if abs(dg(x)) > 1 :
-        print("A sequência irá divergir.")
-    else:
-        while k <= max_int and Erel >= Err:
-            k += 1 # Aumento do contador
-            x = p_novo # Atual iteração k 
-            p_novo = g(x) #Cálculo da nova iteração k+1 
-            Delta = abs(p_novo - x) # Diferença em g(x) ~ Erro Absoluto
-            Erel = (2*Delta)/abs(p_novo) + Err #Erro Relativo
+  if abs(dg(x)) > 1 :
+    print("A sequência irá divergir.")
+  else:
+    while k <= max_int:
+      k += 1 # Aumento do contador
+      x = p_novo # Atual iteração k 
+      p_novo = g(x) #Cálculo da nova iteração k+1 
+      if abs(p_novo - x) < Err or abs(f(p_novo)) < Err: #Critérios de parada
         print("A raiz pelo método do Ponto Fixo é aproximadamnete: ", p_novo)
+        break
 
-g = lambda x: math.cos(x)
-dg = lambda x : -math.sin(x)
+'''f = lambda x: 1 - math.cos(x) #1
+g = lambda x : math.cos(x)
+'''
+'''f = lambda x: x**3 - x - 1 
+g = lambda x: math.pow(x+1,1/3)
+#x0 = 1  E = 0.001'''
+
+f = lambda x: math.exp(-x**2) - math.cos(x) 
+g = lambda x: x + math.cos(x) - math.exp(-x**2) #x0 = [1,2] E= 0.0001
 
 ponto_fixo()
