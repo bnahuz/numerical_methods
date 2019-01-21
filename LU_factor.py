df2d87baf7e7051dc0152274309337fb4680b7e7
Breno Nahuz <35977737+bnahuz@users.noreply.github.com>
2019-01-21 19:03:57 -0300

    Update LU_factor.py

--- LU_factor.py
+++ LU_factor.py
@@ -1,42 +1,47 @@
 def identity(n):
-    m=[[0 for x in range(n)] for y in range(n)]
+    m = [[0 for x in range(n)] for y in range(n)]
     for i in range(0,n):
         m[i][i] = 1
     return m
 
-def gauss(matrix):
-    n = len(matrix)
+def LU_factor(U_array):
+    n = len(U_array)
     m_array = []
     L_array = identity(n)
 
     for k in range (n):
-        if matrix[k][k] == 0:
+        if U_array[k][k] == 0:
             print("Elemento nulo na posição do pivô")
             break
         else:
             for i in range(k + 1,n):
-                m = -matrix[i][k]/matrix[k][k] #Coeficiente para "zerar" os elementos abaixo do pivô e reorganizar os valores da linha.
+                m = -U_array[i][k]/U_array[k][k] #Coeficiente para "zerar" os elementos abaixo do pivô e reorganizar os valores da linha.
                 m_array.append(m)
-                L_array[i][k] = m
-                for j in range(k-1 ,n): #Percorre a linha alterando os valores.
-                    matrix[i][j] = matrix[i][j] + m * matrix[k][j]
-    
-
-    return matrix,L_array
+                L_array[i][k] = -m
+                for j in range(k,n): #Percorre a linha alterando os valores.
+                    U_array[i][j] = U_array[i][j] + m * U_array[k][j]
 
 
-A = [[1,1,1],[2,1,-1],[3,2,0]]
+    print("Matriz U : ")
+    for x in U_array:
+        print(*x, sep=" ")
+
+    #print(m_array)
+    print("Matriz L : ")
+    for x in L_array:
+        print(*x, sep=" ")
+
+    return U_array,L_array
+
+
+
+
+A = [[1,1],[2,3]]
 
 print("Matriz A : ")
 for x in A:
     print(*x, sep=" ")
 
-U_matrix,L_matrix = gauss(A)
+U_array,L_array = LU_factor(A)
 
-print("Matriz L : ")
-for x in L_matrix:
-    print(*x, sep=" ")
 
-print("Matriz U : ")
-for x in U_matrix:
-    print(*x, sep=" ")
